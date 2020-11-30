b2736ea0affe49e19340984b639f34e190d0e5cd
Thibaut <thibautcuchet@hotmail.com>
2020-11-30 11:43:01 +0100

    Add routes /answer

--- src/Components/MapPage.js
+++ src/Components/MapPage.js
@@ -7,9 +7,12 @@ import { blinkItem } from "../utils/render";
 let mapPage = `<div id="mapid"></div>`;
 let percent = 0;
 let questionInterval;
+let data;
+let canClick = false;
 
-const MapPage = async (data) => {
+const MapPage = async (_data) => {
   console.log("Map", data);
+  data = _data;
   let page = document.querySelector("#main");
   page.innerHTML = mapPage;
 
@@ -28,7 +31,7 @@ function loadMap(data) {
   }).setView([51.505, -0.09], 2.5);
 
   let geojson = L.geoJSON(MAP_DATA, {
-    onEachFeature: null,
+    onEachFeature: onEachFeature,
     style: {
       color: "#A2A28B",
       fillOpacity: 1,
@@ -72,31 +75,51 @@ const setQuestionLayout = () => {
 const setProgress = (percent) => {
   let progress = document.querySelector("#progress");
   progress.style.width = `${percent}%`;
-  if (percent > 95) {
+  if (percent == 90) {
     progress.className = "progress-bar bg-danger";
-  } else if (percent > 75) {
+  } else if (percent == 50) {
     progress.className = "progress-bar bg-warning";
-  } else {
-    progress.className = "progress-bar";
+  } else if (percent == 0) {
+    progress.className = "progress-bar bg-info";
   }
 };
 
 const nextQuestion = () => {
+  clearInterval(questionInterval);
   setTimeout(() => {
+    document.querySelector(".question").querySelector("div").innerHTML = "";
     percent = 0;
     setProgress(percent);
   }, TIME_BETWEEN_QUESTION / 2);
-  setTimeout(() => {
-    questionInterval = setInterval(() => {
-      if (percent >= 100) {
-        setTimeout(() => wrongAnswer(), TIME_BETWEEN_QUESTION / 3);
-        clearInterval(questionInterval);
-        nextQuestion();
-      }
-      percent++;
-      setProgress(percent);
-    }, TIME_TO_ANSWER / 100);
-  }, TIME_BETWEEN_QUESTION);
+
+  fetch("http://localhost/api/questions/next", {
+    method: "POST",
+    body: JSON.stringify({
+      location: data.map,
+      type: Object.keys(data).filter((item) => data[item] === true),
+      username: "Thib",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      setTimeout(() => {
+        canClick = true;
+        document.querySelector(".question").querySelector("div").innerHTML =
+          response.question;
+        questionInterval = setInterval(() => {
+          if (percent >= 100) {
+            setTimeout(() => wrongAnswer(), TIME_BETWEEN_QUESTION / 3);
+
+            nextQuestion();
+          }
+          percent++;
+          setProgress(percent);
+        }, TIME_TO_ANSWER / 100);
+      }, TIME_BETWEEN_QUESTION);
+    });
 };
 
 const successAnswer = () => {
@@ -111,6 +134,35 @@ const wrongAnswer = () => {
     duration: 500,
     iterations: 3,
   });
+};
+
+const onEachFeature = (feature, layer) => {
+  layer.on({
+    click: onCountryClick,
+  });
+};
+
+const onCountryClick = (e) => {
+  if (!canClick) return;
+  canClick = false;
+  let selected = e.target.feature.properties.iso2;
+  fetch("http://localhost/api/questions/answer", {
+    method: "POST",
+    body: JSON.stringify({ answer: selected }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response) {
+        successAnswer();
+        nextQuestion();
+      } else {
+        wrongAnswer();
+        nextQuestion();
+      }
+    });
 };
 
 export default MapPage;
