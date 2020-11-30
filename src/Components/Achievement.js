316e629cd305f2507a227a7f1cb45c00db3449e7
Thibaud <thibaud.d.walggraeve@student.vinci.be>
2020-11-30 14:47:08 +0100

    add - achievement page

---
+++ src/Components/Achievement.js
@@ -0,0 +1,18 @@
+import { setTitle } from "../utils/render.js";
+
+const AchievementPage = () => {
+    setTitle("Achievement");
+    WelcomeMessage();
+}
+let page = document.querySelector("#main");
+const WelcomeMessage = () => {
+  const element = document.createElement("p");
+  element.className = "connectionTitle";
+  element.innerHTML = `<h1><strong>WELCOME ON THE ACHIEVEMENT PAGE</strong></h1>
+     Here you can login yourself or create a new account`;
+  element.style.width = "50%";
+  element.style.textAlign = "center";
+  page.append(element);
+};
+
+export default AchievementPage;
