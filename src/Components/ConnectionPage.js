5c41eb0d83588293a2a3d6a431ee5e1e6c9381a8
Thibaut <thibautcuchet@hotmail.com>
2020-11-30 12:01:09 +0100

    Add flex properties to connection form

--- src/Components/ConnectionPage.js
+++ src/Components/ConnectionPage.js
@@ -1,26 +1,26 @@
 import { setTitle } from "../utils/render.js";
 const ConnectionPage = () => {
-    setTitle("Connection");
-    WelcomeMessage();
-    CreateFormConnection();
-  };
-  let page = document.querySelector("#main");
+  setTitle("Connection");
+  WelcomeMessage();
+  CreateFormConnection();
+};
+let page = document.querySelector("#main");
 const WelcomeMessage = () => {
-    const element = document.createElement("p");
-    element.className = "connectionTitle";
-    element.innerHTML = `<h1><strong>WELCOME ON THE CONNECTION PAGE</strong></h1>
+  const element = document.createElement("p");
+  element.className = "connectionTitle";
+  element.innerHTML = `<h1><strong>WELCOME ON THE CONNECTION PAGE</strong></h1>
      Here you can login yourself or create a new account`;
-    element.style.width = "50%";
-    element.style.textAlign = "center";
-    page.append(element);
-  };
+  element.style.width = "50%";
+  element.style.textAlign = "center";
+  page.append(element);
+};
 
 const CreateFormConnection = () => {
   const element = document.createElement("div");
   element.className = "formConnection";
   element.innerHTML = `<div class="wrapper fadeInDown">
   <div id="formContent">
-    
+    <div style="display: flex; flex-direction: column; justify-content: space-between; flex: 1">
     <!-- Login Form -->
     <h5>USER LOGIN</h5>
     <form>
@@ -32,10 +32,11 @@ const CreateFormConnection = () => {
     <div id="formFooter">
       <a class="underlineHover" href="#">Forgot Password?</a>
     </div>
+    </div>
     
-
+    <div style="height: 100%; width: 5px; background: black"></div>
     <!-- Register Form -->
-    
+    <div style="display: flex; flex-direction: column; justify-content: space-between; flex: 1">
     <h5>USER REGISTER</h5>
     <form>
       <input type="text" id="login" class="fadeIn second" name="register" placeholder="login">
@@ -43,10 +44,12 @@ const CreateFormConnection = () => {
       <input type="text" id="password" class="fadeIn third" name="register" placeholder="password">
       <input type="submit" class="fadeIn five" value="Register">
     </form>
-    
+    <div id="formFooter">
+    </div>
+    </div>
   </div>
 </div>`;
   page.append(element);
-}
+};
 
-  export default ConnectionPage;
+export default ConnectionPage;
