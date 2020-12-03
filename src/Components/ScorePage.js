76f8a37e97303aee7b74a51b0c99dc78cce1736c
Thibaud <thibaud.d.walggraeve@student.vinci.be>
2020-12-03 21:14:02 +0100

    end - scorePage design

--- src/Components/ScorePage.js
+++ src/Components/ScorePage.js
@@ -22,84 +22,141 @@ const WelcomeMessage = () => {
   const ScoreBoard = () => {
       const element = document.createElement("div");
       element.className = "score";
+      element.style.width = "65%";
       element.innerHTML = `
       <div id="ScoreContent">
-        <div style="display: flex; flex-direction: column; justify-content: space-between; flex: 1; padding:20px">
-        <h4> SCOREBOARD </h4>
-        <table class="table table-striped">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col" colspan="2">Pseudo</th>
-            <th scope="col"></th>
-            <th scope="col">Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td colspan="2">MaxLaMenace</td>
-            <td></td>
-            <td>6500</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td colspan="2">MaxLaTerreur</td>
-            <td></td>
-            <td>6250</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="8">Timmy</td>
-            <td></td>
-            <td>5700</td>
-          </tr>
-        </tbody>
-      </table>
+        <div style="display: flex; flex-direction: column; flex: 1; padding:20px">
+            <h4> SCOREBOARD </h4>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">
+                                                #                  
+                        </th>
+                        <th scope="col" colspan="8">
+                                                Pseudo                  
+                        </th>
+                        <th scope="col">
+                                                Points                  
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">
+                                                1                  
+                        </th>
+                        <td colspan="8">
+                                                MaxLaMenace                  
+                        </td>
+                        <td>
+                                                6500                  
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                                                2                  
+                        </th>
+                        <td colspan="8">
+                                                MaxLaTerreur                  
+                        </td>
+                        <td>
+                                                6250                  
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                                                3                  
+                        </th>
+                        <td colspan="8">
+                                                Timmy                  
+                        </td>
+                        <td>
+                                                5700                  
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                                                4                  
+                        </th>
+                        <td colspan="8">
+                                                Joseph                  
+                        </td>
+                        <td>
+                                                5550                  
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                                                5                  
+                        </th>
+                        <td colspan="8">
+                                                Thiybeaultd                  
+                        </td>
+                        <td>
+                                                5400                  
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-        <div style="width: 5px; background: #bababa"></div>
+        <div style="width: 5px; background: #D3D3D3"></div>
         <!-- RESULT -->
-        <div style="display: flex; flex-direction: column; justify-content: space-between; flex: 1; padding:20px">
-        <h4> RESULT OF THE GAME </h4>
-        <table class="table table-striped">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col"></th>
-            <th scope="col">Categorie</th>
-            <th scope="col"></th>
-            <th scope="col">Nombre trouvé</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row"></th>
-            <td>Flags found</td>
-            <td></td>
-            <td>5/10</td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td>Countries found</td>
-            <td></td>
-            <td>8/10</td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td>Capitals found</td>
-            <td></td>
-            <td>7/10</td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td>ISO code found</td>
-            <td></td>
-            <td>2/10</td>
-          </tr>
-        </tbody>
-      </table>
-      
+        <div style="display: flex; flex-direction: column; flex: 1; padding:20px">
+            <h4> RESULT OF THE GAME </h4>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col" colspan="8">
+                            Categorie
+                        </th>
+                        <th scope="col">
+                            Nombre trouvé
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="8">
+                            Flags found
+                        </td>
+                        <td class="text-center">
+                            5/10
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="8">
+                            Countries found
+                        </td>
+                        <td class="text-center">
+                            8/10
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="8">
+                            Capitals found
+                        </td>
+                        <td class="text-center">
+                            7/10
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="8">
+                            ISO codes found
+                        </td>
+                        <td class="text-center">
+                            2/10
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div id="yourScore">
+                    <strong>Your score: 3000 points</strong> <br>
+                    HighScore: 3500 points
+                    <input type="submit" class="boutonRejouer" value="Play Again">
+            </div>
         </div>
-      </div>`;
+    </div>`;
       element.style.fontSize = "20px";
       page.append(element);
   }
