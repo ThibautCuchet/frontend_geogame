0e3f58f659f3afb20da126ee7cb50a6cb6e624eb
Thibaud <thibaud.d.walggraeve@student.vinci.be>
2020-12-10 15:53:18 +0100

    add -  navbar modification

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,14 +2,12 @@ import HomePage from "./HomePage.js";
 import ErrorPage from "./ErrorPage.js";
 import MapPage from "./MapPage.js";
 import Connection from "./ConnectionPage.js";
-import Achievement from "./Achievement.js";
 import ScorePage from "./ScorePage.js";
 
 const routes = {
   "/": HomePage,
   "/map": MapPage,
   "/connection": Connection,
-  "/achievement": Achievement,
   "/scorepage": ScorePage,
 };
 
